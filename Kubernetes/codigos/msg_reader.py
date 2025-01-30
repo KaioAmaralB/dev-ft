1f9a0145d9deaccd0de50509a57734d5bc2c91d7
Pedro Christo <pedroegcj@hotmail.com>
2025-01-30 13:42:17 -0300

    Update msg reader code

--- Kubernetes/codigos/msg_reader.py
+++ Kubernetes/codigos/msg_reader.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 import oci
 import json
 import base64
@@ -5,6 +6,13 @@ import os
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
 
 service_endpoint = os.environ['SERVICE_ENDPOINT']
 queue_id = os.environ['QUEUE_ID']
@@ -20,7 +28,7 @@ try:
 
     queue_client = oci.queue.QueueClient(config={}, signer=signer, service_endpoint=service_endpoint)
 except Exception as ex:
-    print(ex)
+    logging.error(ex)
 
 def get_text_secret(secret_ocid):
     try:
@@ -28,7 +36,7 @@ def get_text_secret(secret_ocid):
         secret_content = client.get_secret_bundle(secret_ocid).data.secret_bundle_content.content.encode('utf-8')
         decrypted_secret_content = base64.b64decode(secret_content).decode("utf-8")
     except Exception as ex:
-        logging.getLogger().error("ERROR: failed to retrieve the secret content", ex, flush=True)
+        logging.error("ERROR: failed to retrieve the secret content", ex, flush=True)
         raise
     return decrypted_secret_content
 
@@ -44,10 +52,10 @@ def ensure_collection_exists(db, collection_name):
     """Check if a MongoDB collection exists and create it if it doesn't."""
 
     if collection_name in db.list_collection_names():
-        logging.getLogger().info(f"Collection '{collection_name}' already exists.")
+        logging.info(f"Collection '{collection_name}' already exists.")
     else:
         db.create_collection(collection_name)
-        logging.getLogger().info(f"Collection '{collection_name}' has been created.")
+        logging.info(f"Collection '{collection_name}' has been created.")
 
 def get_message(queue_id):
     
@@ -66,14 +74,21 @@ def delete_message(queue_id, messages):
     return delete_message_response.headers
 
 def insert_data(db ,data):
-    
     db.devopsft.insert_one(json.loads(data))
-    logging.getLogger().info("Dados Inseridos")
+    logging.info("Dados Inseridos")
+
+def check_message_is_json(messages):
+    try:
+        json.loads(messages.content)
+        return True
+    except json.JSONDecodeError as ex:
+        logging.error(f"Menssagem não é Json: {ex}")
+        return False
    
 def process_data(mydb, messages, queue_id):
     insert_data(mydb, messages.content)
     delete_message(queue_id, messages)
-
+   
 if __name__ == "__main__":
 
     passwd = get_text_secret(secret_id)
@@ -86,4 +101,5 @@ if __name__ == "__main__":
         messages = get_message(queue_id)
         if len(messages) > 0:
             for msg in messages:
-                process_data(db, msg, queue_id)
+                if check_message_is_json(msg):
+                    process_data(db, msg, queue_id)
